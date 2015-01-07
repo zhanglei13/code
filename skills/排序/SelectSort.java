ac77e755ff83e0aa50c33a0b484a583430bab21d
zhanglei13 <zhanglei13@otcaix.iscas.ac.cn>
2015-01-07 15:45:34 +0800

    some code

---
+++ skills/排序/SelectSort.java
@@ -0,0 +1,35 @@
+package sort;
+
+/**
+ * Created by zhanglei on 2015/1/6.
+ * 选择排序
+ * 基本思想：在要排序的一组数中，选出最小的一个数与第一个位置的数交换，然后在剩下的数当中再找最小的与第二个位置的数交换，
+ * 如此循环到倒数第二个数和最后一个数比较为止。
+ */
+
+public class SelectSort implements Solution {
+    @Override
+    public int[] sort(int[] num) {
+        int index = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            index = i;
+
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[j] < num[index])
+                    index = j;
+            }
+
+            if (index != i)
+                swap(num, i, index);
+        }
+
+        return num;
+    }
+
+    private void swap(int[] num, int i, int j) {
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+}
